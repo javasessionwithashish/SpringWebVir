9ae467b85cd523439afd3f2df62f9839a109da60
ashish <ashish.koirala004@gmail.com>
2026-07-16 08:24:34 +0545

    jul16commit

--- src/main/java/io/virinchi/springweb/Controller/SignupLoginController.java
+++ src/main/java/io/virinchi/springweb/Controller/SignupLoginController.java
@@ -1,7 +1,9 @@
 package io.virinchi.springweb.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupLoginController {
@@ -16,6 +18,20 @@ return "signupPage";
 public String login()
 {
 return "loginPage";
+}
+
+
+@PostMapping("/signup")
+    public String signupPost(HttpServletRequest request)
+{
+    //request.getParameter("username") name?->Intellij ->form name
+ String username=   request.getParameter("username");
+   String password= request.getParameter("password");
+
+    System.out.println(username);
+    System.out.println(password);
+
+    return "loginPage";
 }
 
 
