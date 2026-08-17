f970bcbf29b364eaf0a35a68914de2b1297b8afa
ashish <ashish.koirala004@gmail.com>
2026-08-17 07:54:43 +0545

    CRUD

--- src/main/java/io/virinchi/springweb/Controller/UserController.java
+++ src/main/java/io/virinchi/springweb/Controller/UserController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,5 +38,22 @@ public class UserController {
 m.addAttribute("user",user);
     return "editPage";
 }
+
+
+@PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute UserTbl user , Model m)
+{
+    //@ModelAttribute-> if the parameters like id,username,password matches
+    //exactly with the model's table.
+    //IN our case, our model UserTbl also has id, username,password that is coming
+    //exactly from editPage.html, we use @ModelAttribute
+
+    uRepo.save(user);
+    //It uses the id parameter to update its username, we can directly use uRepo.save function
+
+    m.addAttribute("totalUsers",uRepo.findAll());
+    return "home";
+}
+
 
 }
