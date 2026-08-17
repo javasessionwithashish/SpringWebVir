2b3e8e827d7d9b9a3c5af74b35964f75d71f92cf
ashish <ashish.koirala004@gmail.com>
2026-08-17 08:08:50 +0545

    Image

---
+++ src/main/java/io/virinchi/springweb/Model/ImgTable.java
@@ -0,0 +1,22 @@
+package io.virinchi.springweb.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ImgTable {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+
+@Lob
+private String image;
+
+//BLOB is used for image management
+//We will encode our image file into string using Base64 Encoder.
+//However, mysql still wont be able to hold string coming from image file
+//even after encoding as the string will be huge.
+//Hence, as mysql can store humongous strings as BLOB- Binary Large Objects
+//Our mysql column will use the idea of BLOB -> as above, @Lob.
+
+
+}
